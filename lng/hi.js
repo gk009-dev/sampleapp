69d4ab27eb19c387f6961755f4ca03edc204d410
gk009-dev <84702979+gk009-dev@users.noreply.github.com>
2021-06-22 22:37:25 +0530

    Add files via upload

---
+++ lng/hi.js
@@ -0,0 +1,9 @@
+export default {
+
+MY_ACCOUNT : 'मेरा खाता',
+MY_HOSPITALS : 'मेरे अस्पताल' ,
+  HOSPITAL_TIMINGS :'अस्पताल का समय',
+SET_OFF_DATES :'तिथियां निर्धारित करें',
+HELP_SUPPORT :'मदद समर्थन',
+ABOUT_US :'हमारे बारे में',
+};
