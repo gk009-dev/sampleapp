1cb949a382afab782d634e1ce481e19d826d2daa
gk009-dev <84702979+gk009-dev@users.noreply.github.com>
2021-06-22 22:42:55 +0530

    Add files via upload

---
+++ en.js
@@ -0,0 +1,9 @@
+export default {
+
+  MY_ACCOUNT:'My Account',
+  MY_HOSPITALS:'My Hospitals',
+  HOSPITAL_TIMINGS:'Hospital Timings',
+  SET_OFF_DATES:'Set Off Dates'
+  HELP_SUPPORT:'Help / Support'
+  ABOUT-US:'About Us'
+};
